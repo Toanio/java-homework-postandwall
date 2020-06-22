19b6b052f06c55a815ef4cf82784ecb33d7ffe45
Чан Тоан <c.toan@corp.mail.ru>
2020-06-22 12:28:46 +0300

    Исправления по домашней работе 2

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,13 +4,13 @@ import ru.netology.domain.Post;
 
 public class PostManager {
     private Post[] posts;
-    private Post[] delete;
 
-    public Post[] delete() {
-        return null;
+
+    public void delete(int ownerId, int postId) {
+
     }
 
-    public Post[] search() {
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
         return null;
     }
     
